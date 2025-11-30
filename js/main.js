2a8d4d332e7828a9406ec37f2f69c36d149931e7
gonyani <greengnome65@gmail.com>
2025-11-30 18:37:07 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-document.getElementById("goEditorBtn").addEventListener("click", () => {
-    const fileInput = document.getElementById("imageInput");
-    const file = fileInput.files[0];
+const uploadBtn = document.getElementById('uploadBtn');
+const imageInput = document.getElementById('imageInput');
 
-    if (!file) {
-        alert("이미지를 먼저 업로드해주세요!");
-        return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = function (e) {
-        localStorage.setItem("uploadedImage", e.target.result);
-        window.location.href = "editor.html";
-    };
-    reader.readAsDataURL(file);
+uploadBtn.addEventListener('click', () => {
+  imageInput.click();
 });
 
+imageInput.addEventListener('change', () => {
+  const file = imageInput.files[0];
+  if (!file) {
+    alert("이미지를 선택해주세요!");
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    localStorage.setItem("uploadedImage", e.target.result);
+    window.location.href = "editor.html";
+  };
+  reader.readAsDataURL(file);
+});
